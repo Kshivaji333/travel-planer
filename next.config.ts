bc79698f658af21ddbcfe00ede04e4bf1cb835b6
Kshivaji333 <ks3653817@gmail.com>
2025-10-09 07:47:53 +0530

    remove esling

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        hostname: "emvqx800eo.ufs.sh"
-      }
-    ]
-  }
+        hostname: "emvqx800eo.ufs.sh",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
